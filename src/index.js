1f09d1ba093dd393b47c5d51560623e3c496fd09
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2026-03-21 22:51:12 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+import { compareAsc, format } from "date-fns";
+
+let year = 2026;
+let month = 3
+let day = 14
+
+let date = format(new Date(year, month -1, day), "dd/MM-yy");
+
+console.log(date);
